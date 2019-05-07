e10fbe78c55e9b8628939c941b2e0145541f9f08
gavinbarrett <gbarrett@ucdavis.edu>
2019-05-07 16:05:14 -0700

    wrote double negation function

--- asb.py
+++ asb.py
@@ -2,35 +2,55 @@
 import sys
 from collections import defaultdict
 
-def negate(string):
-    ''' Return the logical negation of a formula '''
-    return '¬(' + string + ')'
+class LindenbaumConstruction:
 
-def explode():
-    ''' Returns whether formula would cause system to explode '''
+    def __init__(self, f1, f2):
+        # create hashtable to handle unseen formulae 
+        self.Γ = open(f1)
+        self.A = open(f2)
+        self.Δ = defaultdict(lambda: 0)
 
-def lindenbaum(Γ, A):
-    ''' Construct delta superset from gamma '''
+    def negate(self, formula):
+        ''' Return the logical negation of a formula '''
+        return '¬(' + string + ')'
 
-    # create hashtable to handle unseen formulae 
-    Δ = defaultdict(lambda: 0)
+    def doubleNegate(self, f)
+        ''' Returns the formula stripped of negation and parens '''
+        return f[2:len(f)-1]
 
-    # init delta; Δ_0 = Γ
-    for i.strip() in Γ:
-        Δ[i] = 1
-
-    # add sentences from set A to Δ
-    for i.strip() in A:
-        if not explode():
-            Δ[i] = 1
+    def explode(self, f):
+        ''' Returns whether formula would cause system to explode '''
+        if formula[0] == '¬':
+            #FIXME: add formulae correctly
+        if self.Δ[negate(f)] == 1:
+            print('Formula ' + formula + ' cannot consistently be added')
+            return
         else:
-            Δ[negate(i)] = 1
-    f1.close()
+            # handle adding consistent negated formula
+
+    def lindenbaum(self, Γ, A):
+        ''' Construct delta superset from gamma '''
+
+        # init delta; Δ_0 = Γ
+        for i.strip() in Γ:
+            self.Δ[i] = 1
+
+        # add sentences from set A to Δ
+        for i.strip() in self.A:
+            if not self.explode(i):
+                self.Δ[i] = 1
+            else:
+                self.Δ[negate(i)] = 1
+    
+    def die(self):
+        ''' End construction; close files '''
+        self.file1.close()
+        self.file2.close()
+        
 
 
 if __name__ == "__main__":
-    file1 = sys.argv[1]
-    file2 = sys.argv[2]
-    f1 = open(file1)
-    f2 = open(file2)
+
+    lc = LindenbaumConstruction(sys.argv[1], sys.argv[2])
+
     lindenbaum(f1)
